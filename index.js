caad32ef9575ec28bd7e72c2972121500dba07d8
gabenn <52204063+gabenn@users.noreply.github.com>
2020-09-11 19:52:51 +0200

    Big Update
    1. added modal insted of alert
    2. added "animated" favicon
    3. changed lose() and levelWin() into loseWin()
    4. trapX and trapY are no longer global variables
    5. changed event.keycodes into event.key
    6. added production mode
    7. created variables
    -intervalTime
    -moveLength
    -trapsStart

--- index.js
+++ index.js
@@ -1,167 +1,181 @@
-//
-const playerCharacter = document.getElementById("playerCharacter");
-const score = document.getElementById("score");
-let highScore, intervalLeft, intervalDown, intervalRight, intervalUp, traps, gameScore;
+(() => {
+    //links
+    const playerDown = './assets/playerDown.png';
+    const playerLeft = './assets/playerLeft.png';
+    const playerRight = './assets/playerRight.png';
+    //
+    const favIcon = document.querySelector("#favIcon");
+    const playerCharacter = document.querySelector("#playerCharacter");
+    const score = document.querySelector("#score");
+    const modalBox = document.querySelector(".modalBox");
+    const modalHighScore = document.querySelector("#modalHighScore");
+    const modalScore = document.querySelector("#modalScore");
+    //variables
+    const traps = [];
+    const intervalTime = 15;
+    const moveLength = 3;
+    const prodMode = false;
+    const trapsStart = 40;
+    let intervalLeft, intervalDown, intervalRight, intervalUp, gameScore = 0;
 
-if (localStorage.getItem('highScoreLS') == undefined) localStorage.setItem('highScoreLS', 0);
 
-highScore = parseInt(localStorage.getItem('highScoreLS'))
-gameScore = 0;
+    if (localStorage.getItem('highScoreLS') == undefined) localStorage.setItem('highScoreLS', 0);
 
-function lose() {
-    traps.forEach(trap => {
-        //hitboxes (Ik they sucks sometimes)
-        if (trap.offsetTop < playerCharacter.offsetTop + 16 &&
-            trap.offsetTop > playerCharacter.offsetTop - 24 &&
-            trap.offsetLeft < playerCharacter.offsetLeft + 24 &&
-            trap.offsetLeft > playerCharacter.offsetLeft - 60
-        ) {
-            if (highScore == undefined || highScore < gameScore) localStorage.setItem('highScoreLS', gameScore);
+    const highScore = parseInt(localStorage.getItem('highScoreLS'));
 
+    function loseWin() {
+        for (let i = 0; i < traps.length; i++) {
+            if (traps[i].offsetTop < 0) { //changing position when trap is unvisible 
+                traps[i].style.top = `${Math.floor(Math.random() * window.innerHeight)+600}px`;
+                let trapX, trapY;
+                trapX = Math.floor(Math.random() * window.innerWidth);
+
+                if (trapX < 65) trapX += 64;
+                if (trapX > innerWidth - 65) trapX -= 64;
+
+                traps[i].style.left = `${trapX}px`;
+
+                if (i % traps.length == 0) createTrap(trapX, trapY); // adding new element after 
+
+                gameScore = traps.length - trapsStart; //score
+                score.innerHTML = `Score ${gameScore}`;
+            }
+            //hitboxes
+            if (traps[i].offsetTop < playerCharacter.offsetTop + 32 &&
+                traps[i].offsetTop > playerCharacter.offsetTop - 32 &&
+                traps[i].offsetLeft < playerCharacter.offsetLeft + 32 &&
+                traps[i].offsetLeft > playerCharacter.offsetLeft - 64
+            ) {
+                if (highScore == undefined || highScore < gameScore) localStorage.setItem('highScoreLS', gameScore);
+
+                clearIntervals();
+                modalBox.style.zIndex = 3;
+                modalHighScore.value = localStorage.getItem('highScoreLS');
+                modalScore.value = gameScore;
+                // alert(`High Score: ${localStorage.getItem('highScoreLS')}\nScore: ${gameScore}`);
+                // location.reload();
+            }
+        }
+    }
+
+    function moveUp() { //"dev" function :D           
+        intervalUp =
+            setInterval(function () {
+                traps.forEach(trap => {
+                    trap.style.top = `${trap.offsetTop+moveLength}px`;
+                });
+                loseWin();
+            }, intervalTime)
+    }
+
+    function moveDown() {
+        intervalDown =
+            setInterval(function () {
+                traps.forEach(trap => {
+                    trap.style.top = `${trap.offsetTop-moveLength}px`;
+                });
+                loseWin();
+            }, intervalTime)
+    }
+
+    function moveRight() {
+        intervalRight =
+            setInterval(function () {
+                traps.forEach(trap => {
+                    trap.style.top = `${trap.offsetTop-moveLength}px`;
+                    trap.style.left = `${trap.offsetLeft-moveLength}px`;
+                });
+                loseWin();
+            }, intervalTime)
+    }
+
+    function moveLeft() {
+        intervalLeft =
+            setInterval(function () {
+                traps.forEach(trap => {
+                    trap.style.top = `${trap.offsetTop-moveLength}px`;
+                    trap.style.left = `${trap.offsetLeft+moveLength}px`;
+                });
+                loseWin();
+            }, intervalTime)
+    }
+
+    function createTrap(trapX, trapY) {
+        // trap styles
+        const treeTrap = document.createElement('div');
+        treeTrap.className = "traps";
+        treeTrap.id = "deadTree";
+        treeTrap.style.left = `${trapX}px`;
+        treeTrap.style.top = `${trapY}px`;
+        treeTrap.style.backgroundImage = "url('assets/deadTree.png')";
+        //create trap
+        document.body.appendChild(treeTrap);
+        //pushing trap to array
+        traps.push(treeTrap);
+    }
+
+    function clearIntervals() { //"dev" function :D   
+        clearInterval(intervalDown);
+        clearInterval(intervalRight);
+        clearInterval(intervalLeft);
+        clearInterval(intervalUp);
+    }
+    function usingDevFunction() {
+        console.log("Dont be cheater. Dont use dev functions");
+    }
+    document.addEventListener("keydown", (e) => {
+        if (e.key == " ") { //space
+            if (prodMode === true) {
+                clearIntervals();
+                usingDevFunction();
+            }
+        }
+        if (e.key == "ArrowUp" || e.key == "w") { //up
+            if (prodMode === true) {
+                clearIntervals();
+                moveUp();
+                usingDevFunction();
+            }
+        }
+        if (e.key == "ArrowLeft" || e.key == "a") { //left
             clearIntervals();
-            alert(`High Score: ${localStorage.getItem('highScoreLS')}\nScore: ${gameScore}`);
-            location.reload();
+            moveLeft();
+            playerCharacter.style.backgroundImage = `url("${playerLeft}")`;
+            favIcon.href = playerLeft;
         }
 
-    });
-}
+        if (e.key == "ArrowRight" || e.key == "d") { //right
+            clearIntervals();
+            moveRight();
+            playerCharacter.style.backgroundImage = `url("${playerRight}")`;
+            favIcon.href = playerRight;
+        }
 
-function levelWin() {
-    for (let i = 0; i < traps.length; i++) {
-        if (traps[i].offsetTop < 0) { //changing position when trap is unvisible 
+        if (e.key == "ArrowDown" || e.key == "s") { //down
+            clearIntervals();
+            moveDown();
+            playerCharacter.style.backgroundImage = `url("${playerDown}")`;
+            favIcon.href = playerDown;
+        }
+    })
 
-            traps[i].style.top = `${Math.floor(Math.random() * window.innerHeight)+600}px`;
+
+
+    function init() {
+        for (let i = 0; i < trapsStart; i++) { // Traps quantity
+            let trapX, trapY;
             trapX = Math.floor(Math.random() * window.innerWidth);
 
             if (trapX < 65) trapX += 64;
             if (trapX > innerWidth - 65) trapX -= 64;
 
-            traps[i].style.left = `${trapX}px`;
+            trapY = Math.floor(Math.random() * window.innerHeight)
 
-            if (i % traps.length == 0) createTrap(); // adding new element after "level win"
-
-            gameScore = traps.length - 40;; //score
-            score.innerHTML = `Score ${gameScore}`;
+            while (trapY < window.innerHeight * 0.5) {
+                trapY = Math.floor(Math.random() * window.innerHeight);
+            }
+            createTrap(trapX, trapY);
         }
     }
-}
-
-function moveUp() { //"dev" function :D           
-    intervalUp =
-        setInterval(function () {
-            for (let i = 0; i < traps.length; i++) {
-                traps[i].style.top = `${traps[i].offsetTop+3}px`;
-            }
-            lose();
-            levelWin();
-        }, 15)
-}
-
-function moveDown() {
-    intervalDown =
-        setInterval(function () {
-            for (let i = 0; i < traps.length; i++) {
-                traps[i].style.top = `${traps[i].offsetTop-3}px`;
-            }
-            lose();
-            levelWin();
-        }, 15)
-}
-
-function moveRight() {
-    intervalRight =
-        setInterval(function () {
-            for (let i = 0; i < traps.length; i++) {
-                traps[i].style.top = `${traps[i].offsetTop-3}px`;
-                traps[i].style.left = `${traps[i].offsetLeft-3}px`;
-            }
-            lose();
-            levelWin();
-        }, 15)
-}
-
-function moveLeft() {
-    intervalLeft =
-        setInterval(function () {
-            for (let i = 0; i < traps.length; i++) {
-                traps[i].style.top = `${traps[i].offsetTop-3}px`;
-                traps[i].style.left = `${traps[i].offsetLeft+3}px`;
-            }
-            lose();
-            levelWin();
-        }, 15)
-}
-
-function clearIntervals() { //"dev" function :D   
-    clearInterval(intervalDown);
-    clearInterval(intervalRight);
-    clearInterval(intervalLeft);
-    clearInterval(intervalUp);
-}
-
-function usingDevFunction() {
-    console.log("Dont be cheater. Dont use dev functions");
-}
-
-function createTrap() {
-    // trap styles
-    treeTrap = document.createElement('div');
-    treeTrap.className = "traps";
-    treeTrap.id = "deadTree";
-    treeTrap.style.left = `${trapX}px`;
-    treeTrap.style.top = `${trapY}px`;
-    treeTrap.style.backgroundImage = "url('assets/deadTree.png')";
-    //create trap
-    document.body.appendChild(treeTrap);
-    //trap array
-    traps = document.getElementsByClassName("traps");
-    traps = [...traps];
-}
-document.addEventListener("keydown", function (event) {
-    if (event.keyCode == 32) { //"dev" function :D space
-        clearIntervals();
-        usingDevFunction();
-    }
-    if (event.keyCode == 38 || event.keyCode == 87) { //"dev" function :D up
-        clearIntervals();
-        moveUp();
-        usingDevFunction();
-    }
-    if (event.keyCode == 37 || event.keyCode == 65) { //left
-        clearIntervals();
-        moveLeft();
-        playerCharacter.style.backgroundImage = 'url("./assets/playerLeft.png")';
-    }
-
-    if (event.keyCode == 39 || event.keyCode == 68) { //right
-        clearIntervals();
-        moveRight();
-        playerCharacter.style.backgroundImage = 'url("./assets/playerRight.png")';
-    }
-
-    if (event.keyCode == 40 || event.keyCode == 83) { //down
-        clearIntervals();
-        moveDown();
-        playerCharacter.style.backgroundImage = 'url("./assets/playerDown.png")';
-    }
-})
-
-let trapX, trapY, treeTrap;
-
-function init() {
-    for (let i = 0; i < 40; i++) { // Traps quantity
-
-        trapX = Math.floor(Math.random() * window.innerWidth);
-
-        if (trapX < 65) trapX += 64;
-        if (trapX > innerWidth - 65) trapX -= 64;
-
-        trapY = Math.floor(Math.random() * window.innerHeight)
-
-        while (trapY < window.innerHeight * 0.5) {
-            trapY = Math.floor(Math.random() * window.innerHeight);
-        }
-        createTrap();
-    }
-}
-init();
+    init();
+})();
